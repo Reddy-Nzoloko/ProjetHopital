948912fe05e6d1840f21eb01804177e5825a2a57
Reddy-Nzoloko <reddynzoloko@gmail.com>
2026-03-21 16:38:30 +0200

    Update script.sql
    Treads

--- ProjetHopital/script.sql
+++ ProjetHopital/script.sql
@@ -3,5 +3,4 @@ create table hopital;
 create database hopital client; 
 create view selections donnee 
 create triggers before insert 
-crate view janvier 
-ngoma nene ngoma nene eeh 
+create treads 
