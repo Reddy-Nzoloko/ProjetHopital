8009761d0501bac2594959303d4a06887659a22c
Reddy-Nzoloko <reddynzoloko@gmail.com>
2026-06-16 13:28:51 +0200

    Update script.sql
    Day

--- ProjetHopital/script.sql
+++ ProjetHopital/script.sql
@@ -1,4 +1,4 @@
-rupture sans systeme exploitation create database hopital 
+forztt is daybrupture sans systeme exploitation create database hopital 
 create table hopital; 
 create database hopital client; 
 create view selections donnee 
