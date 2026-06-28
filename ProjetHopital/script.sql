36fc48bc86fd2b482e4fa0f84e5b0ec9ee57761a
Reddy-Nzoloko <reddynzoloko@gmail.com>
2026-06-28 10:06:49 +0200

    Update script.sql
    Si ero

--- ProjetHopital/script.sql
+++ ProjetHopital/script.sql
@@ -1,4 +1,4 @@
-sabatsaboto edit fils come back to luk longue day satan un autre day salon leopart forztt is daybrupture sans systeme exploitation create database hopital 
+repositoru sabatsaboto edit fils come back to luk longue day satan un autre day salon leopart forztt is daybrupture sans systeme exploitation create database hopital 
 create table hopital; 
 create database hopital client; 
 create view selections donnee 
