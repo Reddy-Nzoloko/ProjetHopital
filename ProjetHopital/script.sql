39f3bb99d9f9fff096a56ea35c3f35716eba1235
Reddy-Nzoloko <reddynzoloko@gmail.com>
2026-07-02 18:51:19 +0200

    Update script.sql
    ASUS

--- ProjetHopital/script.sql
+++ ProjetHopital/script.sql
@@ -1,4 +1,4 @@
-ifodi repositoru sabatsaboto edit fils come back to luk longue day satan un autre day salon leopart forztt is daybrupture sans systeme exploitation create database hopital 
+edite file ifodi repositoru sabatsaboto edit fils come back to luk longue day satan un autre day salon leopart forztt is daybrupture sans systeme exploitation create database hopital 
 create table hopital; 
 create database hopital client; 
 create view selections donnee 
