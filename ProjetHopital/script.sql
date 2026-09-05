93bf4791a1aef5aa32e6855a79b777f6b9f2371d
Reddy-Nzoloko <reddynzoloko@gmail.com>
2026-09-05 12:58:20 +0200

    Update script.sql
    Azo

--- ProjetHopital/script.sql
+++ ProjetHopital/script.sql
@@ -1,4 +1,4 @@
-saurris pardon 
+120$ saurris pardon 
 peche mageur statlong dimanche sport 
 brayab muss encore pfo sudo vendredi edite file ifodi repositoru sabatsaboto edit fils come back to luk longue day satan un autre day salon leopart forztt is daybrupture sans systeme exploitation create database hopital 
 create table hopital; 
