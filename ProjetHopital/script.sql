775a9ff8b602b4d6d2c0554ee1e975b28ba04d7f
Reddy-Nzoloko <reddynzoloko@gmail.com>
2026-09-10 16:53:50 +0200

    Update script.sql
    Pere

--- ProjetHopital/script.sql
+++ ProjetHopital/script.sql
@@ -1,4 +1,4 @@
-top repositoey 
+instrumentolo top repositoey 
 newd objectif 120$ saurris pardon 
 peche mageur statlong dimanche sport 
 brayab muss encore pfo sudo vendredi edite file ifodi repositoru sabatsaboto edit fils come back to luk longue day satan un autre day salon leopart forztt is daybrupture sans systeme exploitation create database hopital 
